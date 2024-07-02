173c09f13dce293d673a7ebdaaacf3a189c14e33
jaoovp <112101270+ratodichava@users.noreply.github.com>
2024-07-02 13:51:37 -0300

    ...

---
+++ script.js
@@ -0,0 +1,19 @@
+const caixaPrincipal = document . querySelector ( '.caixa-principal' ) ;
+const caixapergunta = document . querySelector ( '.caixa-pergunta' ) ;
+const caixaAlternativas = document . querySelector ( '.caixa-alternativas' ) ;
+const caixaResultado = document . querySelector ( '.caixa-resultados' ) ;
+const caixaResultado = document . querySelector ( '.caixa-resultados' ) ;
+
+const perguntas = [ //abre lista de perguntas
+    { //abre objeto de perguntas
+        enunciado : "pergunta 1" ,
+        alternativas : [
+            alternativa 1 " ,
+        enunciado : "pergunta 1" , 
+        alternativas : [ 
+            " alternativa 1" ,
+            "alternativa 2"
+        ]
+    } //busca objeto de perguntas
+    } //busca objeto de perguntas 
+] //busca lista de perguntas
